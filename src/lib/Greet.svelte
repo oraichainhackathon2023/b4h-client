3f8ccdf4d39f969226bcacd2f869e8863c4a668e
ducnm <mducbg2000@gmail.com>
2023-04-01 16:03:48 +0700

    Init

---
+++ src/lib/Greet.svelte
@@ -0,0 +1,22 @@
+<script>
+	import { invoke } from '@tauri-apps/api/tauri';
+
+	let name = '';
+	let greetMsg = '';
+
+	async function greet() {
+		greetMsg = await invoke('greet', { name });
+	}
+</script>
+
+<div>
+	<input
+		type="text"
+		class="input input-bordered input-primary w-full max-w-xs"
+		id="greet-input"
+		placeholder="Enter a name..."
+		bind:value={name}
+	/>
+	<button class="btn btn-primary" on:click={greet}>Greet</button>
+	<p>{greetMsg}</p>
+</div>
